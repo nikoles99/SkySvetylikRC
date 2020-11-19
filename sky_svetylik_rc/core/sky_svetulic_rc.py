9d040c439f0a5a8c5c5f786370a82a7ee0e7b83a
nikita <nikido@tut.by>
2020-11-19 17:43:50 +0300

    add min/max pulse width restrictions

--- sky_svetylik_rc/core/sky_svetulic_rc.py
+++ sky_svetylik_rc/core/sky_svetulic_rc.py
@@ -1,6 +1,8 @@
+from constants.constants import GAS_MAX, GAS_MIN, YAW_MAX, YAW_MIN, PITCH_MAX, PITCH_MIN, ROLL_MAX, ROLL_MIN
 from utils.config_utils import ConfigUtils
 
 MIDDLE_PULSE_WIDTH = 1500
+
 
 class SkySvetylicRC:
 
@@ -13,18 +15,39 @@ class SkySvetylicRC:
         pass
 
     def update(self, transmitter):
-        yaw_percents = self.compute_in_persents(transmitter.yaw_pw)
-        pitch_percents = self.compute_in_persents(transmitter.pitch_pw)
-        roll_percents = self.compute_in_persents(transmitter.roll_pw)
-        gas_forward_left = transmitter.gas_pw + (transmitter.gas_pw * yaw_percents) + (transmitter.gas_pw * pitch_percents) + (transmitter.gas_pw * roll_percents)
-        gas_forward_right = transmitter.gas_pw - (transmitter.gas_pw * yaw_percents) + (transmitter.gas_pw * pitch_percents) - (transmitter.gas_pw * roll_percents)
-        gas_backward_left = transmitter.gas_pw - (transmitter.gas_pw * yaw_percents) - (transmitter.gas_pw * pitch_percents) + (transmitter.gas_pw * roll_percents)
-        gas_backward_right = transmitter.gas_pw + (transmitter.gas_pw * yaw_percents) - (transmitter.gas_pw * pitch_percents) - (transmitter.gas_pw * roll_percents)
-        print("lv={} lh={} rh={} rl{}".format(gas_forward_left, gas_forward_right, gas_backward_left, gas_backward_right))
+        yaw_percents = self.compute_in_percents(transmitter.yaw_pw, YAW_MIN, YAW_MAX)
+        pitch_percents = self.compute_in_percents(transmitter.pitch_pw, PITCH_MIN, PITCH_MAX)
+        roll_percents = self.compute_in_percents(transmitter.roll_pw, ROLL_MIN, ROLL_MAX)
+
+        gas_forward_left = transmitter.gas_pw + (transmitter.gas_pw * yaw_percents) + (transmitter.gas_pw * pitch_percents) + (
+                transmitter.gas_pw * roll_percents)
+        gas_forward_right = transmitter.gas_pw - (transmitter.gas_pw * yaw_percents) + (transmitter.gas_pw * pitch_percents) - (
+                transmitter.gas_pw * roll_percents)
+        gas_backward_left = transmitter.gas_pw - (transmitter.gas_pw * yaw_percents) - (transmitter.gas_pw * pitch_percents) + (
+                transmitter.gas_pw * roll_percents)
+        gas_backward_right = transmitter.gas_pw + (transmitter.gas_pw * yaw_percents) - (transmitter.gas_pw * pitch_percents) - (
+                transmitter.gas_pw * roll_percents)
+        print("{}   {}   {}   {}".format(yaw_percents, pitch_percents, roll_percents, gas_backward_right))
         self.gas(gas_forward_left, gas_forward_right, gas_backward_left, gas_backward_right)
         pass
 
     def gas(self, forward_left, forward_right, backward_left, backward_right):
+        if forward_left > GAS_MAX:
+            forward_left = GAS_MAX
+        if forward_left < GAS_MIN:
+            forward_left = GAS_MIN
+        if forward_right > GAS_MAX:
+            forward_right = GAS_MAX
+        if forward_right < GAS_MIN:
+            forward_right = GAS_MIN
+        if backward_left > GAS_MAX:
+            backward_left = GAS_MAX
+        if backward_left < GAS_MIN:
+            backward_left = GAS_MIN
+        if backward_right > GAS_MAX:
+            backward_right = GAS_MAX
+        if backward_right < GAS_MIN:
+            backward_right = GAS_MIN
         self.board.set_servo_pulsewidth(self.ESC_FORWARD_LEFT_GPIO, forward_left)
         self.board.set_servo_pulsewidth(self.ESC_FORWARD_RIGHT_GPIO, forward_right)
         self.board.set_servo_pulsewidth(self.ESC_BACKWARD_LEFT_GPIO, backward_left)
@@ -42,8 +65,10 @@ class SkySvetylicRC:
     def yaw(self):
         pass
 
-    def compute_in_persents(self, yaw_pulse_width):
-        if yaw_pulse_width == MIDDLE_PULSE_WIDTH:
-            return yaw_pulse_width - MIDDLE_PULSE_WIDTH
+    @staticmethod
+    def compute_in_percents(current_pw, min_pw, max_pw):
+        average_pw = (max_pw + min_pw) / 2
+        if current_pw == average_pw:
+            return 0
         else:
-            return (yaw_pulse_width - MIDDLE_PULSE_WIDTH) * 0.2 / 100
+            return (current_pw - average_pw) * 0.002
